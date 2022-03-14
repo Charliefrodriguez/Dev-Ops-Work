96fa655781991c0220e0039382109b01deea8c70
Charles Rodriguez <charliefrodriguez@gmail.com>
2022-03-13 21:50:17 -0400

    library restructure

--- Dockerfile.groovy
+++
@@ -1,6 +0,0 @@
-FROM groovy:jdk11-alpine  
-RUN mkdir groovy_work
-WORKDIR groovy_work
-USER root
-RUN apk update
-RUN apk add vim
--- start_groovy_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curr_path=$(pwd)
-
-docker run -it -v $curr_path/groovy_work:/home/groovy/groovy_work groovy_env:latest /bin/sh 
