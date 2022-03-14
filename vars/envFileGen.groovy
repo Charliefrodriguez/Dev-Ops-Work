636a46baa71b64360b41fa893d535e654561335f
Charles Rodriguez <charliefrodriguez@gmail.com>
2022-03-14 07:47:04 -0400

    path

--- vars/envFileGen.groovy
+++ vars/envFileGen.groovy
@@ -4,7 +4,7 @@ def call(String file_name){
 	def jsonSlurper = new JsonSlurperClassic()
 	data = jsonSlurper.parse(new File(file_name)) 
 	println(data)
-	File output =  new File("output.env") 
+	File output =  new File("$WORKSPACE/output.env") 
 	for(key in data){ 
 	   output.append(key)
 	   output.append("\n")
